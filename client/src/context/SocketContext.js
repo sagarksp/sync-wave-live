1f6955540553ee3eae9213ce97648961d3414670
sagarksp <111245337+sagarksp@users.noreply.github.com>
2026-06-10 12:40:11 +0530

    Update SocketContext.js

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -10,7 +10,7 @@ export function SocketProvider({ children, accountId, deviceName }) {
 
   useEffect(() => {
     if (!accountId) return;
-    const socket = io("http://localhost:3001", { transports: ["websocket"] });
+    const socket = io("https://syncwave-server-live.onrender.com/", { transports: ["websocket"] });
     socketRef.current = socket;
 
     socket.on("connect", () => {
